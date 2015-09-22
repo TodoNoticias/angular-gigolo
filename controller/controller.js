1490af6a9df3cd16828f7eddbdae23af48830c57
Gaston Briones <mauricio_briones@artear.com.ar>
2015-09-22 18:00:24 -0300

    merge

--- controller/controller.js
+++ controller/controller.js
@@ -22,13 +22,13 @@ App.controller('Login', ['$scope', '$http', '$rootScope', '$timeout', function (
         }).success(function (data) {
             $timeout(function () {
                 $rootScope.isLogged = Boolean(data);
+                $rootScope.executeTimer();
             }, 2000)
         });
     }
 }]);
 
-
-App.controller('Question', ['$scope', '$http', '$rootScope', '$timeout', function ($scope, $http, $rootScope, $timeout) {
+App.controller('Question', ['$scope', '$http', '$rootScope', function ($scope, $http, $rootScope) {
 
     $http.get('json/questions.json').success(function (data) {
 
@@ -45,22 +45,20 @@ App.controller('Question', ['$scope', '$http', '$rootScope', '$timeout', functio
         $scope.response = function (response) {
             if (response === data[$scope.currentIndex].answer) {
                 $scope.currentIndex++;
-                $rootScope.win =  $scope.questions.length == $scope.currentIndex ? true : false;
-                $rootScope.user.user_time = $scope.time;
+                $rootScope.win = $scope.questions.length == $scope.currentIndex ? true : false;
+                $rootScope.user.user_time = $rootScope.user_time;
                 update_usr($rootScope.user);
-
 
             } else {
                 $rootScope.fail = true;
                 $rootScope.user.user_status = 0;
+                $rootScope.user.user_time = $rootScope.user_time;
                 update_usr($rootScope.user);
             }
         }
     });
 
-    function update_usr(data){
-        console.log('Esta es la data que llea ---> ',data)
-
+    function update_usr(data) {
         $http({
             url: 'php-scripts/update.php',
             method: 'POST',
@@ -68,6 +66,38 @@ App.controller('Question', ['$scope', '$http', '$rootScope', '$timeout', functio
             headers: {'content-type': 'application/json'}
         }).success(function (data) {
             $rootScope.isUpdated = true;
+            $rootScope.stopTimer();
         });
+    }
+}]);
+
+App.controller('Timer', ['$scope', '$rootScope', '$interval', function ($scope,$rootScope, $interval) {
+
+    $scope.cent = 0;
+    $scope.seg = 0;
+    $scope.min = 0;
+    var interval;
+
+    $scope.onTimeout = function(){
+        $scope.cent++;
+
+        if($scope.cent == 99){
+            $scope.cent = 0;
+            $scope.seg++;
+            $scope.seg = ($scope.seg).toString().length < 2 ? '0'+$scope.seg : $scope.seg
+        }
+        if($scope.seg == 60){
+            $scope.seg = 0
+            $scope.min++
+        }
+    }
+
+    $rootScope.executeTimer = function (){
+       interval = $interval($scope.onTimeout,10);
+    }
+
+    $rootScope.stopTimer = function(){
+        $interval.cancel(interval);
+        $rootScope.user_time = $scope.min +':'+$scope.seg +':'+$scope.cent
     }
 }]);
--- php-scripts/update.php
+++ php-scripts/update.php
@@ -3,10 +3,8 @@ include 'connect.php';
 
 $data = json_decode(file_get_contents('php://input'));
 
-echo "esto es data desde el php --->  ".json_encode($data);
-
 $sql = "UPDATE users
-        SET user_status='$data->user_status'
+        SET user_status='$data->user_status', user_time='$data->user_time'
         WHERE user_name='$data->user_name'";
 
 if (mysqli_query($conn, $sql)) {
